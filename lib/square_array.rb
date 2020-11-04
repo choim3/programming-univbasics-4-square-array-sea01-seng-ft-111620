e88d61bbceb4d89c83db526b628ee2ad1e11ea92
gentle-expression-4880 <choim3@spu.edu>
2020-11-04 05:44:54 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   counter = 0;
   while counter < square_array.length do
-   new_array = square_array[counter] ** 2;
+   new_array[] = square_array[counter] ** 2;
     puts new_array
     counter += 1;
   end
